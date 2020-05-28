832c36de2f95d73e0ea8732592eeedddd2f2810c
Jamar Torres <Jetorres88@gmail.com>
2020-05-28 15:15:29 -0400

    created Data.js to maintain preset data

---
+++ team-builder/src/Data.js
@@ -0,0 +1,37 @@
+export default [
+    {
+        name: "Jamar", 
+        email: "jamar@gmail.com", 
+        role: "Developer"
+    },
+    {
+        name: "Ryan", 
+        email: "Ryan@aol.com", 
+        role: "Chef"
+    },
+    {
+        name: "Juan", 
+        email: "Juan@msn.com", 
+        role: "Finance Guy"
+    },
+    {
+        name: "Sam", 
+        email: "Sam@hotmail.com", 
+        role: "Politician"
+    },
+    {
+        name: "Paul", 
+        email: "Paul@email.co", 
+        role: "Developer"
+    },
+    {
+        name: "Oliver",
+        email: 'Oliver@segmanmax.io',
+        role: 'IT supervisor'
+    },
+    {
+        name: 'Dimmi',
+        email: 'dimmi@gmail.com',
+        role: 'Astrologer'
+    }
+]
